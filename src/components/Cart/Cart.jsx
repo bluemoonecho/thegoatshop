84317584e921e8dbcbffa4db2c72bb014ddb28e3
Sabine Holler <sabidasa@gmail.com>
2021-05-19 15:58:07 +0200

    cart item

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Container, Typography, Button, Grid} from '@material-ui/core'
 import useStyles from './styles'
-
+import CartItem from './CartItem/CartItem'
 
 const Cart = ({cart}) => {
 
@@ -17,7 +17,7 @@ const Cart = ({cart}) => {
         <Grid container spacing={3}>
             {cart.line_items.map((item) => (
                 <Grid item xs={12} sm={4} key={item.id}>
-                    <div> {item.name} </div>
+                    <CartItem item={item}/>
                 </Grid>
             ))}
         </Grid>
@@ -26,8 +26,8 @@ const Cart = ({cart}) => {
                     Subtotal: {cart.subtotal.formatted_with_symbol}
                     </Typography>
                     <div>
-                        <Button className={classes.emptyButton} size='large' type='button' varient='contained' color='secondary'>Empty Cart</Button>
-                        <Button className={classes.checkoutButton} size='large' type='button' varient='contained' color='primary'>Check Out</Button>
+                        <Button className={classes.emptyButton} size='large' type='button' variant='contained' color='secondary'>Empty Cart</Button>
+                        <Button className={classes.checkoutButton} size='large' type='button' variant='contained' color='primary'>Check Out</Button>
                     </div>
         </div>
         </>
@@ -38,12 +38,12 @@ const Cart = ({cart}) => {
     return (
         <Container>
             <div className={classes.toolbar}/>
-            <Typography className={classes.title} varient='h3'>
+            <Typography className={classes.title} variant='h4' gutterBottom>
                 Your Shopping Cart
             </Typography>
             {!cart.line_items.length ? <EmptyCart/> : <FilledCart/>}
         </Container>
-    ) 
+    )  
 }
 
 export default Cart;
